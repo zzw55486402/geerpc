bc0b3c5ba57e986d1e703dfb4b4da7659a69c4b9
zzw55486402 <55095949+zzw55486402@users.noreply.github.com>
2021-08-21 18:15:15 +0800

    Create README.md

--- xclient/xclient.go
+++ xclient/xclient.go
@@ -1,6 +1,8 @@
 package xclient
 
 import (
+	"context"
+	. "geerpc" // 调用的时候可以省略包的名字
 	"io"
 	"sync"
 )
@@ -13,23 +15,54 @@ type XClient struct {
 	clients map[string]*Client
 }
 
-
 var _ io.Closer = (*XClient)(nil)
 
 func NewXClient(d Discovery, mode SelectMode, opt *Option) *XClient {
-	return &XClient{d:d, mode: mode, opt: opt, clients: make(map[string]*Client}
+	return &XClient{d: d, mode: mode, opt: opt, clients: make(map[string]*Client)}
 }
 
 func (xc *XClient) Close() error {
 	xc.mu.Lock()
 	defer xc.mu.Unlock()
-	for key, client := range xc.clients{
+	for key, client := range xc.clients {
 		_ = client.Close()
 		delete(xc.clients, key)
 	}
 	return nil
 }
 
-func (xc *XClient) dial(rpcAddr string) (*Client, error)  {
-	
+func (xc *XClient) dial(rpcAddr string) (*Client, error) {
+	xc.mu.Lock()
+	defer xc.mu.Unlock()
+	client, ok := xc.clients[rpcAddr]
+	if ok && !client.IsAvailable() {
+		_ = client.Close()
+		delete(xc.clients, rpcAddr)
+		client = nil
+	}
+	if client == nil {
+		var err error
+		client, err = XDial(rpcAddr, xc.opt)
+		if err != nil {
+			return nil, err
+		}
+		xc.clients[rpcAddr] = client
+	}
+	return client, nil
+}
+
+func (xc *XClient) call(rpcAddr string, ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	client, err := xc.dial(rpcAddr)
+	if err != nil {
+		return err
+	}
+	return client.Call(ctx, serviceMethod, args, reply)
+}
+
+func (xc *XClient) Call(ctx context.Context, serviceMethod string, args, reply interface{}) error {
+	rpcAddr, err := xc.d.Get(xc.mode)
+	if err != nil {
+		return err
+	}
+	return xc.call(rpcAddr, ctx, serviceMethod, args, reply)
 }
